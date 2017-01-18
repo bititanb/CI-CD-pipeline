910d1cd1ace5c618a115070013307e8f7fbdefb2
bititanb <bititanc@gmail.com>
2017-01-18 04:04:34 +0300

    upd

--- django/tasks/urls.py
+++ django/tasks/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 
 from tasks import views
 
-# TODO
-#urlpatterns = [
-#    url(r'^)
-#]
+urlpatterns = [
+    url(r'^$', views.TaskList.as_view(), name='tasklist'),
+    url(r'^add/$', views.TaskAdd.as_view(), name='taskadd'),
+    url(r'^(?P<pk>\d+)/$', views.TaskUpdate.as_view(), name='taskupdate'),
+    url(r'^(?P<pk>\d+)/delete/$', views.TaskDelete.as_view(), name='taskdelete'),
+]
--- django/tasks/views.py
+++ django/tasks/views.py
@@ -1,2 +1,27 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.views import generic
+#from django.shortcuts import render
 
+from tasks import models, forms
+
+class TaskList(generic.ListView):
+    model = models.Task
+    template_name = 'tasks/tasklist.html'
+    fields = '__all__'
+
+class TaskAdd(generic.CreateView):
+    model = models.Task
+    template_name = 'tasks/taskadd.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasklist')
+
+class TaskUpdate(generic.UpdateView):
+    model = models.Task
+    template_name = 'tasks/taskupdate.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasklist')
+
+class TaskDelete(generic.DeleteView):
+    model = models.Task
+    template_name = 'tasks/taskdelete.html'
+    success_url = reverse_lazy('tasklist')
