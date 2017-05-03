9351d1581acd4a70c272e39f8ffb3a8fd35603e6
bititanb.home <bititanc@gmail.com>
2017-05-04 00:40:57 +0300

    upd

--- django/taskmngr/settings.py
+++ django/taskmngr/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
                 'ENGINE': 'django.db.backends.postgresql',
                 'NAME': 'postgres',
                 'USER': 'postgres',
-                'HOST': 'postgres',
+                'HOST': 'localhost',
                 'PORT': 5432,
     }
 }
