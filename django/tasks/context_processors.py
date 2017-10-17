11d5761991dfc3733fbc86480f76e99afb37a2ad
bititanb.home <bititanc@gmail.com>
2017-10-17 04:06:16 +0300

    upd

--- django/tasks/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-
-def app_version(request):
-    return {'app_version': settings.APP_VERSION}
-
-def app_revision_short(request):
-    return {'app_revision_short': settings.APP_REVISION_SHORT}
-
--- django/tasks/static/tasks/js/custom.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    // autosize textareas
-    autosize($('textarea'));
-
-    // change placeholders for empty fields
-    $( 'div.form-group-formset.empty' ).find( "textarea[id*='body'], input[id*='title']"  ).attr("placeholder", "Add new");
-})
-
-
