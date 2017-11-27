16a4167b22a1a7e1045627efcf9d05350d9681bb
unknown <bititanc@gmail.com>
2017-11-27 16:38:33 +0300

    fixed hosts.bat

--- scripts/hosts.bat
+++ scripts/hosts.bat
@@ -1,32 +1,36 @@
 @echo off
+SETLOCAL
 
-goto check_Permissions
-:check_Permissions
-    net session >nul 2>&1
-    if %errorLevel% NEQ 0 (
-        echo Please, run with admin privileges.
-        echo Exiting.
-        goto END
-    )
+set HOSTS_FILE=%WINDIR%\system32\drivers\etc\hosts
 
-find /C /I "taskmngr1" %WINDIR%\system32\drivers\etc\hosts >NUL 2>NUL
-if %ERRORLEVEL% NEQ 0 (
-  echo.
-  echo 192.168.59.2 taskmngr1 >> %WINDIR%\System32\drivers\etc\hosts
+net session >nul 2>&1
+if %errorLevel% NEQ 0 (
+	echo Please, run with admin privileges.
+	echo Exiting.
+	EXIT /B 1
 )
 
-find /C /I "testing.taskmngr1" %WINDIR%\system32\drivers\etc\hosts >NUL 2>NUL
-if %ERRORLEVEL% NEQ 0 (
-  echo.
-  echo 192.168.59.2 testing.taskmngr1 >> %WINDIR%\System32\drivers\etc\hosts
-)
+:: remove read-only
+attrib -R %WINDIR%\system32\drivers\etc\hosts
 
-find /C /I "taskmngr2" %WINDIR%\system32\drivers\etc\hosts >NUL 2>NUL
-if %ERRORLEVEL% NEQ 0 (
-  echo.
-  echo 192.168.59.3 taskmngr2 >> %WINDIR%\System32\drivers\etc\hosts
-)
+Call :write_hosts "192.168.59.2 taskmngr1" , " taskmngr1"
+Call :write_hosts "192.168.59.2 testing.taskmngr1" , "testing.taskmngr1"
+Call :write_hosts "192.168.59.2 taskmngr2" , "taskmngr2"
 
 echo Finished
-:END
 pause
+EXIT /B 0
+
+:write_hosts
+:: %~1 : new hosts entry
+:: %~2 : write only if this string is not presented in file
+find /C /I "%~2" "%HOSTS_FILE%" >NUL 2>NUL
+if %ERRORLEVEL% NEQ 0 (
+	:: newline
+	echo. >> %HOSTS_FILE%
+	:: actual entry
+	echo %~1 >> %HOSTS_FILE%
+) else (
+	echo String, containing "%~2" already in the "%HOSTS_FILE%". Skipping.
+)
+EXIT /B 0
